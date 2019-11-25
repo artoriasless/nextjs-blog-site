cba0b831b99bd300c9df3a8c4f52a19a3a877d16
stan <tangjianyi1993@live.com>
2019-11-25 14:09:11 +0800

    update: update lib func update the function getParams in lib, return the query key and value in url

--- src/lib/common-get-params.js
+++ src/lib/common-get-params.js
@@ -3,6 +3,16 @@ const getParams = ({ isServer, pathname, asPath, query, }) => {
      * 通用的获取 URL 中 params 参数对象的方法
      */
     const params = {};
+    const queryStr = asPath.split('?')[1] || '';
+
+    queryStr.split('&').forEach(queryParam => {
+        const [
+            paramName = '',
+            paramVal = '',
+        ] = queryParam.split('=');
+
+        params[paramName] = paramVal;
+    });
 
     if (isServer) {
         const pathArr = pathname.split('/');

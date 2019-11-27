8f1b8fb59ddf3bb08e129ce6b832ff23c1fa9522
stan <tangjianyi1993@live.com>
2019-11-27 23:29:46 +0800

    fix: wrong returned data in API util.seo due to the node require cache, the required constant value will be changed after the last modification; update the returned data

--- web/controller/api/util.js
+++ web/controller/api/util.js
@@ -4,22 +4,27 @@ const {
     seoTpl,
 } = require('../../lib/constant');
 
+const copyObj = obj => JSON.parse(JSON.stringify(obj));
+
 const util = {
     seo: async ctx => {
         const reqData = ctx.query || {};
         const pageName = reqData.pageName || '';
-        const data = seoTpl[pageName] || seoTpl.default;
+        const data = copyObj(seoTpl[pageName] || seoTpl.default);
         const result = {
             success: true,
             message: 'fetch seo data success.',
             data: '',
         };
+        let type;   // for catalogue
         
         switch (pageName) {
         case 'catalogue':
-            data.title = data.title.replace(/\[type\]/g, reqData.type || 'all');
+            type = reqData.filterType;
+            type += type === 'all' ? '' : `>${reqData.filterParam}`;
+            data.title = data.title.replace(/\[type\]/g, type);
             data.title = data.title.replace(/\[page\]/g, reqData.page || '1');
-            data.keywords = data.keywords.replace(/\[type\]/g, reqData.type || 'all');
+            data.keywords = data.keywords.replace(/\[type\]/g, type);
             break;
         default:
             // do nothing

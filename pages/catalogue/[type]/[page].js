3d2ce98c16e87ee9ea61a9bb3e09cb0ec3c74713
stan <tangjianyi1993@live.com>
2019-11-27 23:35:52 +0800

    update: complete catalogue page complete and beautify the catalogue page

--- pages/catalogue/[type]/[page].js
+++ pages/catalogue/[type]/[page].js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { withRouter } from 'next/router';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import fetch from 'isomorphic-unfetch';
 
-import config from 'config';
+import {
+    getParams,
+    getFetchUrl,
+} from 'lib';
 import {
     TitleGenerator,
     HeadGenerator,
     Layout,
+    Navbar,
+    LoginModal,
+    FilterCount,
+    PaperCatalogue,
 } from 'components';
 
-const Catalogue = withRouter(function(props) {
-    const seo = props.seo || {};
+import 'style/pages/catalogue.scss';
+
+const UI_Catalogue = function(props) {
+    const {
+        seo,
+        filterCount,
+        catalogue,
+        userInfo,
+        filterType,
+        filterParam,
+    } = props;
 
     return (
         <>
@@ -18,23 +36,82 @@ const Catalogue = withRouter(function(props) {
             <HeadGenerator seo={ seo }/>
             <Layout>
                 <div className="page-catalogue">
-                    <h1>Catalogue Page</h1>
-                    <div>Catalogue page test</div>
+                    <Navbar/>
+                    <div className="page-section-body row">
+                        <FilterCount filterCount={ filterCount }/>
+                        <PaperCatalogue 
+                            catalogue={ catalogue }
+                            userInfo={ userInfo }
+                            filterType={ filterType }
+                            filterParam={ filterParam }
+                        />
+                    </div>
                 </div>
             </Layout>
+            <LoginModal/>
         </>
     );
-});
+};
+const mapState2Props = (state, props) => Object.assign({}, state, props);
+const mapDispatch2Props = () => ({});
+let Catalogue;
 
-Catalogue.getInitialProps = async () => {
-    const seoRes = await fetch(`${config.domain}/api/util/seo?page=catalogue`);
-    const seoResult = await seoRes.json();
+UI_Catalogue.propTypes = {
+    seo: PropTypes.object,
+    filterCount: PropTypes.object,
+    catalogue: PropTypes.object,
+    userInfo: PropTypes.object,
+    filterType: PropTypes.string,
+    filterParam: PropTypes.string,
+};
+Catalogue = connect(
+    mapState2Props,
+    mapDispatch2Props,
+)(UI_Catalogue);
+Catalogue.getInitialProps = async ctx => {
+    const {
+        type,
+        page,
+    } = getParams(ctx);
+    const filterType = type.split('>')[0] || 'unknown';
+    const filterParam = type.split('>')[1] || 'unknown';
+
+    const seoData = {
+        pageName: 'catalogue',
+        filterType,
+        filterParam,
+        page,
+    };
+    const seoReq = getFetchUrl('util.seo', seoData);
+    const seoRes = seoReq && await fetch(seoReq);
+    const seoResult = seoReq && await seoRes.json();
+
+    const catalogueData = {
+        filterType,
+        filterParam,
+        page,
+    };
+    const catalogueReq = getFetchUrl('catalogue.page', catalogueData);
+    const catalogueRes = catalogueReq && await fetch(catalogueReq);
+    const catalogueResult = catalogueReq && await catalogueRes.json();
+
+    const filterCountData = {
+        filterType: 'all',
+    };
+    const filterCountReq = getFetchUrl('paper.filterCount', filterCountData);
+    const filterCountRes = filterCountReq && await fetch(filterCountReq);
+    const filterCountResult = filterCountReq && await filterCountRes.json();
     
     const initProps = {
         seo: seoResult.data,
+        catalogue: catalogueResult.data,
+        filterCount: filterCountResult.data,
+
+        filterType,
+        filterParam,
     };
 
     return initProps;
 };
 
-export default Catalogue;
+export default withRouter(Catalogue);

f743bc39ac67c6095b70b648508b76c115fe92d8
stan <tangjianyi1993@live.com>
2019-12-06 14:08:17 +0800

    update: init pages init some pages: paper-submit, share-paper-submit

--- pages/paper-submit/[id].js
+++ pages/paper-submit/[id].js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import dynamic from 'next/dynamic';
+import React from 'react';
 import { withRouter } from 'next/router';
 import fetch from 'isomorphic-unfetch';
 
@@ -10,120 +9,28 @@ import {
     Layout,
 } from 'components';
 
-const Editor = function() {
-    // https://xdsoft.net/jodit/
-    const JoditEditor = dynamic(() => import('jodit-react'), {
-        ssr: false
-    });
+import 'style/pages/paper-submit.scss';
 
-    if (typeof window === 'undefined') {
-        return null;
-    } else {
-        const paperContent = 'title';
-        const changeHandler = val => {
-            console.info('change', val); // eslint-disable-line
-        };
-        const editorConfig = {
-            readonly: false,
-            controls: {
-                font: {
-                    list: {
-                        'brush script mt': 'Brush Script MT',
-                        'consola': 'Consolas',
-                        'microsoft yahei': 'MicroSoft Yahei',
-                        'snell roundhand script': 'Snell Roundhand Script',
-                    },
-                },
-            },
-            buttons: [
-                'font', 'fontsize', 'brush', 'paragraph', 'align',
-                '|',
-                'bold', 'strikethrough', 'underline', 'italic',
-                '|',
-                'superscript', 'subscript',
-                '|',
-                'ul', 'ol',
-                '|',
-                'outdent', 'indent',
-                '|',
-                'undo', 'redo',
-                '\n',
-                'hr',
-                'eraser',
-                'copyformat',
-                '|',
-                'symbol',
-                'fullsize',
-                '|',
-                'image', 'file', 'video', 'table', 'link',
-                '|',
-            ],
-            extraButtons: [
-                {
-                    name: 'upload',
-                    icon: 'upload',
-                    exec: function (editor) { // eslint-disable-line
-                        console.info('upload'); // eslint-disable-line
-                    }
-                },
-                {
-                    name: 'save',
-                    icon: 'save',
-                    exec: function (editor) { // eslint-disable-line
-                        console.info('save'); // eslint-disable-line
-                    }
-                },
-            ],
-        };
-        const $editorRef = typeof window === 'undefined' ? null : window.jodit;
-
-        return (
-            <JoditEditor
-                editorRef={ $editorRef }
-                value={ paperContent }
-                config={ editorConfig }
-                onChange={ changeHandler }
-            />
-        );
-    }
-};
-const paperSubmit = withRouter(function(props) {
+const PaperSubmit = function(props) {
     const seo = props.seo || {};
-    const assetPrefix = config.dev ? '' : config.ossPublic.assetPrefix;
-
-    useEffect(() => {
-        const intervalTask = setInterval(() => {
-            if (typeof window !== 'undefined' && window.jodit) {
-                clearInterval(intervalTask);
-            }
-        }, 100);
-
-        setTimeout(() => {
-            clearInterval(intervalTask);
-        }, 10000);
-    }, []);
 
     return (
         <>
             <TitleGenerator title={ seo.title }/>
             <HeadGenerator seo={ seo }>
-                <link rel="stylesheet" href={ `${assetPrefix}/static/plugins/jodit/index.css` }/>
-                <script src={ `${assetPrefix}/static/plugins/jodit/index.js` }></script>
+                <meta name="robots" content="nofollow"/>
             </HeadGenerator>
             <Layout>
-                <div className="page-paper-submit">
+                <div className="page-profile">
                     <h1>Paper Submit Page</h1>
                     <div>Paper Submit page test</div>
-                    <div style={{ margin: '25px 50px', }}>
-                        <Editor/>
-                    </div>
                 </div>
             </Layout>
         </>
     );
-});
+};
 
-paperSubmit.getInitialProps = async () => {
+PaperSubmit.getInitialProps = async () => {
     const seoRes = await fetch(`${config.domain}/api/util/seo?pageName=paperSubmit`);
     const seoResult = await seoRes.json();
     
@@ -134,4 +41,4 @@ paperSubmit.getInitialProps = async () => {
     return initProps;
 };
 
-export default paperSubmit;
+export default withRouter(PaperSubmit);

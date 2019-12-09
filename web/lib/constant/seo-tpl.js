e327005655aab100eddafa8d5703bc3a8e5a40e3
stan <tangjianyi1993@live.com>
2019-12-09 15:17:04 +0800

    feat: add SEO tpl add SEO information template about profile and activate page

--- web/lib/constant/seo-tpl.js
+++ web/lib/constant/seo-tpl.js
@@ -18,6 +18,18 @@ const paper = {
     keywords: '前端全栈,技术笔记,[tags]',
 };
 
+const profile = {
+    title: 'Hi, [name], welcome to your personal profile setting page.',
+    description: 'set profile information here, change avatar, modify password and so on...',
+    keywords: 'profile',
+};
+
+const activate = {
+    title: 'Hi, Buddy, visit this page to activate your account.',
+    description: 'this web page is used for activating user\'s account, so that user can submit reply.',
+    keywords: 'activate_account',
+};
+
 const unknown = {
     title: '前端_全栈_技术笔记-屎蛋(Stan)的个人网站',
     description: '屎蛋(Stan)的个人网站，主要记录了工作内外，在前端全栈方面，以及Linux、Java等其他方面的一些的技术笔记。',
@@ -28,5 +40,7 @@ module.exports = {
     home,
     catalogue,
     paper,
+    profile,
+    activate,
     default: unknown,
 };

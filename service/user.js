75dc0a78ee886c92c17004c0577ce371ea4e0555
Artoriasless <tangjianyi1993@live.com>
2022-07-10 17:05:27 +0800

    Update user.js

--- service/user.js
+++ service/user.js
@@ -7,49 +7,49 @@ const models = require('../model');
 const User = models.db.User;
 
 module.exports = {
-    async create(data) {
-        const user = await User.create(data);
+  async create(data) {
+    const user = await User.create(data);
 
-        return (user ? user.toJSON() : {});
-    },
-    async update(data) {
-        const id = data.id;
-        const user = await User.findById(id);
+    return user ? user.toJSON() : {};
+  },
+  async update(data) {
+    const id = data.id;
+    const user = await User.findByPk(id);
 
-        if (user) {
-            const result = await user.update(data);
+    if (user) {
+      const result = await user.update(data);
 
-            return (result ? result.toJSON() : {});
-        }
-        return null;
-    },
-    async findById(id) {
-        const user = await User.findById(id);
+      return result ? result.toJSON() : {};
+    }
+    return null;
+  },
+  async findByPk(id) {
+    const user = await User.findByPk(id);
 
-        return (user ? user.toJSON() : {});
-    },
-    async findMany(query){
-        query = query || {};
+    return user ? user.toJSON() : {};
+  },
+  async findMany(query) {
+    query = query || {};
 
-        const users = await User.findAll(query);
+    const users = await User.findAll(query);
 
-        return users.map(user => (user ? user.toJSON() : {}));
-    },
-    async page(where, page) {
-        where = where || {};
-        page = page || 1;
+    return users.map(user => (user ? user.toJSON() : {}));
+  },
+  async page(where, page) {
+    where = where || {};
+    page = page || 1;
 
-        const query = {
-            where,
-            limit: PAGE_LENGTH,
-            offset: ((page - 1) * PAGE_LENGTH),
-        };
-        const result = await User.findAndCountAll(query);
+    const query = {
+      where,
+      limit: PAGE_LENGTH,
+      offset: (page - 1) * PAGE_LENGTH,
+    };
+    const result = await User.findAndCountAll(query);
 
-        return ({
-            page,
-            count: result.count,
-            rows: result.rows.map(user => (user ? user.toJSON() : {})),
-        });
-    },
+    return {
+      page,
+      count: result.count,
+      rows: result.rows.map(user => (user ? user.toJSON() : {})),
+    };
+  },
 };

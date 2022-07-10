7f80ea859b577e5d309b67352260d418a81cd12a
Artoriasless <tangjianyi1993@live.com>
2022-07-10 17:06:41 +0800

    Update message.js

--- service/message.js
+++ service/message.js
@@ -7,49 +7,49 @@ const models = require('../model');
 const Message = models.db.Message;
 
 module.exports = {
-    async create(data) {
-        const message = await Message.create(data);
+  async create(data) {
+    const message = await Message.create(data);
 
-        return (message ? message.toJSON() : {});
-    },
-    async update(data) {
-        const id = data.id;
-        const message = await Message.findById(id);
+    return message ? message.toJSON() : {};
+  },
+  async update(data) {
+    const id = data.id;
+    const message = await Message.findByPk(id);
 
-        if (message) {
-            const result = await message.update(data);
+    if (message) {
+      const result = await message.update(data);
 
-            return (result ? result.toJSON() : {});
-        }
-        return null;
-    },
-    async findById(id) {
-        const message = await Message.findById(id);
+      return result ? result.toJSON() : {};
+    }
+    return null;
+  },
+  async findByPk(id) {
+    const message = await Message.findByPk(id);
 
-        return (message ? message.toJSON() : {});
-    },
-    async findMany(query){
-        query = query || {};
+    return message ? message.toJSON() : {};
+  },
+  async findMany(query) {
+    query = query || {};
 
-        const replies = await Message.findAll(query);
+    const replies = await Message.findAll(query);
 
-        return replies.map(message => (message ? message.toJSON() : {}));
-    },
-    async page(where, page) {
-        where = where || {};
-        page = page || 1;
+    return replies.map(message => (message ? message.toJSON() : {}));
+  },
+  async page(where, page) {
+    where = where || {};
+    page = page || 1;
 
-        const query = {
-            where,
-            limit: PAGE_LENGTH,
-            offset: ((page - 1) * PAGE_LENGTH),
-        };
-        const result = await Message.findAndCountAll(query);
+    const query = {
+      where,
+      limit: PAGE_LENGTH,
+      offset: (page - 1) * PAGE_LENGTH,
+    };
+    const result = await Message.findAndCountAll(query);
 
-        return ({
-            page,
-            count: result.count,
-            rows: result.rows.map(message => (message ? message.toJSON() : {})),
-        });
-    },
+    return {
+      page,
+      count: result.count,
+      rows: result.rows.map(message => (message ? message.toJSON() : {})),
+    };
+  },
 };

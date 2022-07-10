2252d36a9c58b8e6172569fad9e7954fe5f5daf0
Artoriasless <tangjianyi1993@live.com>
2022-07-10 17:11:42 +0800

    Update index.js

--- model/db/index.js
+++ model/db/index.js
@@ -5,42 +5,87 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 const config = require('../../config');
+const User = require('./user');
+const Paper = require('./paper');
+const Reply = require('./reply');
+const Message = require('./message');
 
 const dbconn = `mysql://${config.db.user}:${config.db.password}@${config.db.host}:${config.db.port}/${config.db.database}`;
 
 const sequelize = new Sequelize(dbconn, config.db.seq_options);
 
 const db = {
-    Sequelize,
-    sequelize,
+  Sequelize,
+  sequelize,
 };
 
-const models = {};
-const filterFunc = fileName => {
-    return (path.extname(fileName) === '.js' && fileName !== 'index.js');
+const models = {
+  User: User(sequelize, Sequelize.DataTypes),
+  Paper: Paper(sequelize, Sequelize.DataTypes),
+  Reply: Reply(sequelize, Sequelize.DataTypes),
+  Message: Message(sequelize, Sequelize.DataTypes),
 };
-fs.readdirSync(__dirname)
-    .filter(filterFunc)
-    .forEach(modelName => {
-        const model = sequelize.import(path.join(__dirname, modelName));
 
-        models[model.name] = model;
-    });
+// 表关联
+// Message
+models.Message.belongsTo(models.User, {
+  foreignKey: 'userId',
+  targetKey: 'id',
+});
+models.Message.belongsTo(models.Paper, {
+  foreignKey: 'paperId',
+  targetKey: 'id',
+});
+models.Message.belongsTo(models.Reply, {
+  foreignKey: 'replyId',
+  targetKey: 'id',
+});
+// Paper
+models.Paper.hasMany(models.Message, {
+  foreignKey: 'paperId',
+  sourceKey: 'id',
+});
+models.Paper.hasMany(models.Reply, {
+  foreignKey: 'paperId',
+  sourceKey: 'id',
+});
+// Reply
+models.Reply.belongsTo(models.User, {
+  foreignKey: 'userId',
+  targetKey: 'id',
+});
+models.Reply.belongsTo(models.Paper, {
+  foreignKey: 'paperId',
+  targetKey: 'id',
+});
+models.Reply.hasMany(models.Message, {
+  foreignKey: 'replyId',
+  as: 'Reply',
+});
+// User
+models.User.hasMany(models.Message, {
+  foreignKey: 'userId',
+  sourceKey: 'id',
+});
+models.User.hasMany(models.Reply, {
+  foreignKey: 'userId',
+  sourceKey: 'id',
+});
 
 Object.keys(models).forEach(key => {
-    const model = models[key];
-    
-    if ('associate' in model) {
-        model.associate(models);
-    }
+  const model = models[key];
 
-    db[key] = model;
+  if ('associate' in model) {
+    model.associate(models);
+  }
+
+  db[key] = model;
 });
 
 if (process.env.NODE_ENV === 'development') {
-    db.sequelize.sync({
-        // force: true, //  for local test,avoid lost test data
-    });
+  db.sequelize.sync({
+    // force: true, //  for local test,avoid lost test data
+  });
 }
 
 module.exports = db;

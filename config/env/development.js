19995241e99320b4f1a643c52a6a1ecbe2150492
Artoriasless <tangjianyi1993@live.com>
2022-07-10 17:12:35 +0800

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -1,22 +1,20 @@
 'use strict';
 
 module.exports = {
-    domain: 'http://127.0.0.1:3100',
-    port: 3100,
-    db: {
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: '12345',
-        database: 'stanby_site',
-        seq_options: {
-            logging: false,
-            dialectOptions: {
-                charset: 'utf8',
-            },
-        },
+  domain: 'http://127.0.0.1:3100',
+  port: 3100,
+  db: {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Tangjianyi1993@',
+    database: 'artoriasless',
+    seq_options: {
+      logging: false,
+      dialectOptions: {
+        charset: 'utf8',
+      },
     },
-    owners: [
-        '4da3a6d7-999d-4955-8ea2-d86ea9cf1234'
-    ],
+  },
+  owners: ['1b8ea9c8-1edc-41fa-b173-7c9f55429fdc'],
 };

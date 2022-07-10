c39179a1b443aba3a412f6e5ca7bd35003efc2d4
Artoriasless <tangjianyi1993@live.com>
2022-07-10 17:12:16 +0800

    Update common.js

--- config/env/common.js
+++ config/env/common.js
@@ -2,36 +2,34 @@
 
 const pkg = require('../../package.json');
 
-const staticFolder = 'stanby-site';
+const staticFolder = 'artoriasless-site';
 const staticVersion = pkg.version;
-const domain = '//monkingstand.oss-cn-beijing.aliyuncs.com';
+const domain = '//artoriasless.oss-cn-hangzhou.aliyuncs.com';
 const assetFolder = `${staticFolder}/${staticVersion}`;
 const userPrefix = `${domain}/user`;
 const assetPrefix = `${domain}/${staticFolder}/${staticVersion}`;
 const paperPrefix = `${domain}/${staticFolder}/paper`;
 
 module.exports = {
-    dev: process.env.NODE_ENV !== 'production',
-    archiving: '浙ICP备19024648号',
-    sessionKeys: [
-        'user',
-    ],
-    session: {
-        key: 'koa-stanby-site-session',
-        maxAge: 86400000,
-        overwrite: true,
-        httpOnly: true,
-        signed: true,
-        rolling: false,
-        renew: false,
-    },
-    ossPublic: {
-        domain,
-        assetFolder,
-        userPrefix,
-        assetPrefix,
-        paperPrefix,
-        staticFolder,
-        staticVersion,
-    },
+  dev: process.env.NODE_ENV !== 'production',
+  archiving: '',
+  sessionKeys: ['user'],
+  session: {
+    key: 'koa-artoriasless-site-session',
+    maxAge: 86400000,
+    overwrite: true,
+    httpOnly: true,
+    signed: true,
+    rolling: false,
+    renew: false,
+  },
+  ossPublic: {
+    domain,
+    assetFolder,
+    userPrefix,
+    assetPrefix,
+    paperPrefix,
+    staticFolder,
+    staticVersion,
+  },
 };

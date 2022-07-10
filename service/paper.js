ff79611e4f29b70355cb462f30dd9ed1f758418d
Artoriasless <tangjianyi1993@live.com>
2022-07-10 17:06:11 +0800

    Update paper.js

--- service/paper.js
+++ service/paper.js
@@ -9,82 +9,70 @@ const models = require('../model');
 const Paper = models.db.Paper;
 
 module.exports = {
-    async create(data) {
-        const paper = await Paper.create(data);
+  async create(data) {
+    const paper = await Paper.create(data);
 
-        return (paper ? paper.toJSON() : {});
-    },
-    async update(data) {
-        const id = data.id;
-        const paper = await Paper.findById(id);
+    return paper ? paper.toJSON() : {};
+  },
+  async update(data) {
+    const id = data.id;
+    const paper = await Paper.findByPk(id);
 
-        if (paper) {
-            const result = await paper.update(data);
+    if (paper) {
+      const result = await paper.update(data);
 
-            return (result ? result.toJSON() : {});
-        }
-        return null;
-    },
-    async findById(id) {
-        const paper = await Paper.findById(id);
+      return result ? result.toJSON() : {};
+    }
+    return null;
+  },
+  async findByPk(id) {
+    const paper = await Paper.findByPk(id);
 
-        return (paper ? paper.toJSON() : {});
-    },
-    async findMany(query){
-        query = query || {};
+    return paper ? paper.toJSON() : {};
+  },
+  async findMany(query) {
+    query = query || {};
 
-        const papers = await Paper.findAll(query);
+    const papers = await Paper.findAll(query);
 
-        return papers.map(paper => (paper ? paper.toJSON() : {}));
-    },
-    async page(where, page) {
-        where = where || {};
-        page = page || 1;
+    return papers.map(paper => (paper ? paper.toJSON() : {}));
+  },
+  async page(where, page) {
+    where = where || {};
+    page = page || 1;
 
-        const query = {
-            where,
-            limit: PAGE_LENGTH,
-            offset: ((page - 1) * PAGE_LENGTH),
-        };
-        const result = await Paper.findAndCountAll(query);
+    const query = {
+      where,
+      limit: PAGE_LENGTH,
+      offset: (page - 1) * PAGE_LENGTH,
+    };
+    const result = await Paper.findAndCountAll(query);
 
-        return ({
-            page,
-            count: result.count,
-            rows: result.rows.map(paper => (paper ? paper.toJSON() : {})),
-        });
-    },
-    async filterCount(filterType) {
-        const filterOpts = {
-            tag: {
-                group: ['tag'],
-                attributes: [
-                    'tag',
-                    sequelize.fn('count', sequelize.col('tag'))
-                ]
-            },
-            timeline: {
-                group: ['year_tag'],
-                attributes: [
-                    ['year_tag', 'yearTag'],
-                    sequelize.fn('count', sequelize.col('year_tag')),
-                ]
-            },
-            latest: {
-                limit: 5,
-                offset: 0,
-                attributes: [
-                    'id',
-                    'title',
-                    'tag',
-                ],
-                order: [
-                    ['id', 'DESC'],
-                ],
-            }
-        };
-        const result = await Paper.findAndCountAll(filterOpts[filterType]);
+    return {
+      page,
+      count: result.count,
+      rows: result.rows.map(paper => (paper ? paper.toJSON() : {})),
+    };
+  },
+  async filterCount(filterType) {
+    const filterOpts = {
+      tag: {
+        group: ['tag'],
+        attributes: ['tag', sequelize.fn('count', sequelize.col('tag'))],
+      },
+      timeline: {
+        group: ['year_tag'],
+        attributes: [['year_tag', 'yearTag'], sequelize.fn('count', sequelize.col('year_tag'))],
+      },
+      latest: {
+        limit: 5,
+        offset: 0,
+        attributes: ['id', 'title', 'tag'],
+        order: [['id', 'DESC']],
+      },
+    };
+    const result = await Paper.findAndCountAll(filterOpts[filterType]);
 
-        return result;
-    },
+    return result;
+  },
 };
